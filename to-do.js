a170944d9ce8c41b48e12b9fd3ea8671a1a0f998
EDISON  SMART <edisonsmart77@gmail.com>
2024-09-18 22:15:50 +0100

    done

---
+++ to-do.js
@@ -0,0 +1,37 @@
+// // sectectors
+// const todoinput = document.querySelector(".todo-input ");
+// const todobutton = document.querySelector(".todo-button");
+// const todolist = document.querySelector(".todo-list")
+
+
+// // Event listemers
+// todoButton.addEventlistener("click", addTodo);
+
+// // fuctions
+// function addtodo(event) {
+//     // prevent form from submitting
+//     event.preventDefault();
+// //  todo Div 
+// const todoDiv = document.createElement("div");
+// todoDiv.classList.add("todo");
+// // create
+// const newTodo = document.createElement("li")
+// newTodo.innerText = "hey";
+// newTodo.classList.add("todo-item");
+// todoDiv.appendChild(newTodo);
+// // check mark button
+// const completedButton = document.createElement("button");
+// completedButton.innerText =  <i class="fas fa-check"></i>;
+// completedButton.classList.add("complete-btn");
+// todoDiv.appendChild(completedButton);
+// // check trash button
+// const trashButton = document.createElement("button");
+// trashButton.innerText =  <i class="fas fa-trash"></i>;
+// trashButton.classList.add("complete-btn");
+// todoDiv.appendChild(trashButton);
+// // append to list
+// todolist.appendChild(todoDiv);
+// }
+
+
+// DOM SELECTORS 
